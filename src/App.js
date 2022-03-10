9d7c2844cb8b94764444db9cc53a83b25750143f
Timmy Liaw <timmy1544@gmail.com>
2022-03-10 11:14:41 -0800

    start to implement redux

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import { createStore } from 'redux';
 import HelloWorld from "./HelloWorld";
 
+const store = createStore(reducer);
+
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+  // const [tech] = useState("Redux");
+  // return <HelloWorld tech={tech} />;
+  return;
 };
 
 export default App;
