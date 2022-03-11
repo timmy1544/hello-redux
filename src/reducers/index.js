b734ba65a7988e4e9b840fcecd6cd8392481f53c
Timmy Liaw <timmy1544@gmail.com>
2022-03-10 19:20:03 -0800

    before text updating

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 const reducer =  (state, action) => {
   switch (action.type) {
     case 'SET_TECHNOLOGY':
+      console.log(action)
       return {
         ...state,
-        tech: action.tach
+        tech: action.tech
       };
     default:
       return state;
