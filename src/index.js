33ba484ab3e9d5b50757ca054b9912c41596d587
Timmy Liaw <timmy1544@gmail.com>
2022-03-11 11:08:40 -0800

    add rerender function to update the text

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => ReactDOM.render(<App />, document.getElementById("root"));
+render();
+store.subscribe(render);
 registerServiceWorker();
